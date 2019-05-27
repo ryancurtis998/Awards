a299027d8351c346e9564af50e62e2bf9d7f7c7b
santa <ryancurtis998@yahoo.com>
2019-05-27 11:39:30 +0300

    updated forms.py

---
+++ main/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import *
+
+class UploadForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        exclude = ('website',)
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+exclude = ('website',)
